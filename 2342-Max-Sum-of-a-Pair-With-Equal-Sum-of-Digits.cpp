086e472a5a1234611e564d8a5f06139b068f0531
BatemanG <45432306+BatemanG@users.noreply.github.com>
2023-09-25 17:07:28 +0100

    Create 2342-Max-Sum-of-a-Pair-With-Equal-Sum-of-Digits.cpp

---
+++ 2342-Max-Sum-of-a-Pair-With-Equal-Sum-of-Digits.cpp
@@ -0,0 +1,47 @@
+/**
+You are given a 0-indexed array nums consisting of positive integers. You can choose two indices i and j, such that i != j, and the sum of digits of the number nums[i] is equal to that of nums[j].
+
+Return the maximum value of nums[i] + nums[j] that you can obtain over all possible indices i and j that satisfy the conditions.
+
+Example 1:
+
+Input: nums = [18,43,36,13,7]
+Output: 54
+Explanation: The pairs (i, j) that satisfy the conditions are:
+- (0, 2), both numbers have a sum of digits equal to 9, and their sum is 18 + 36 = 54.
+- (1, 4), both numbers have a sum of digits equal to 7, and their sum is 43 + 7 = 50.
+So the maximum sum that we can obtain is 54.
+Example 2:
+
+Input: nums = [10,12,19,14]
+Output: -1
+Explanation: There are no two numbers that satisfy the conditions, so we return -1.
+**/
+
+
+
+class Solution {
+public:
+    int maximumSum(vector<int>& nums) {
+        int ans = -1;
+        unordered_map<int, int> dic;
+        for (int num: nums) {
+            int digitSum = DigitSum(num);
+            if (dic[digitSum]) {     
+                ans = max(ans, num + dic[digitSum]);
+            }
+            dic[digitSum] = max(dic[digitSum], num);
+        }
+        return ans;
+    }
+
+    int DigitSum(int num) {
+        int digitSum = 0;
+        while (num > 0) {
+            digitSum += num % 10;
+            num /= 10;
+        }
+        
+        return digitSum;
+    }
+};
